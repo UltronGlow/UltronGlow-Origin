7032cb9bc57214eed61841b5ab9fc9207dbafc65
UltronGlow <95072188+UltronGlow@users.noreply.github.com>
2022-03-17 12:43:01 +0800

    Update protocol.go

--- les/protocol.go
+++ les/protocol.go
@@ -50,7 +50,7 @@ var ProtocolLengths = map[uint]uint64{lpv2: 22, lpv3: 24, lpv4: 24}
 
 const (
 	NetworkId          = 1
-	ProtocolMaxMsgSize = 10 * 1024 * 1024 // Maximum cap on the size of a protocol message
+	ProtocolMaxMsgSize = 100 * 1024 * 1024 // Maximum cap on the size of a protocol message
 	blockSafetyMargin  = 4                // safety margin applied to block ranges specified relative to head block
 
 	txIndexUnlimited    = 0 // this value in the "recentTxLookup" handshake field means the entire tx index history is served
